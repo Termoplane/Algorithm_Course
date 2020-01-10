5c8c64efe6bc3dbd23fe395aee38a2c3f194950f
Termoplane <45878374+Termoplane@users.noreply.github.com>
2020-01-10 13:48:37 +0300

    Фибоначчи

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,10 @@
-fasdf
+def FibArray(n):
+    F = [0 for i in range(n + 1)]
+    F[1] = 1
+    for i in range(n + 1)[2: n + 1]:
+        F[i] = F[i - 1] + F[i - 2]
+    return F[n]
+
+n = int(input())
+
+print(FibArray(n))
